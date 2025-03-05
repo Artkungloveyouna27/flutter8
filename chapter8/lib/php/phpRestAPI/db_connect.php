d71d369774fff5105f234baac28b262231cb4c23
Artkungloveyouna27 <66040233104@udru.ac.th>
2025-03-05 12:16:57 +0000

    update

---
+++ chapter8/lib/php/phpRestAPI/db_connect.php
@@ -0,0 +1,14 @@
+
+<?php
+    $db_name =  "its66040233112";
+    $host = "localhost";
+    $db_user = "its66040233112";
+    $db_pass ="H5tmJ2K8";
+
+    $conn = mysqli_connect($host, $db_user, $db_pass, $db_name);
+    if (!$conn) {
+        die("Connect failed!!");
+    }
+
+    
+?>
